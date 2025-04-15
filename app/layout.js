2d9020d10c94463d6315cb42574118a0b377360f
Salil Thakur <salilthakur09@gmail.com>
2025-04-15 17:19:49 +0530

    added promptBox and clerk modules

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -13,12 +14,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.classname} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.classname} antialiased`}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
