ab2a85a3f1cb050a27aafee2489e31f3491577ea
Salil Thakur <salilthakur09@gmail.com>
2025-04-22 05:34:01 +0530

    fixed errors

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -14,7 +14,7 @@ export async function POST(req){
     };
 
 
-    const payload=awaitreq.json();
+    const payload=await req.json();
     const body=JSON.stringify(payload);
     const {data, type}=wh.verify(body, svixHeaders);
     
